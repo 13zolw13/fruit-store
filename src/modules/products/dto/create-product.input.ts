2ad72de14c5306947ac2c62cb1a17ec43fa8f098
tomek <tomaszpawelkowalski@gmail.com>
2022-05-21 18:31:42 +0200

    feat: generate product interface

--- src/modules/products/dto/create-product.input.ts
+++ src/modules/products/dto/create-product.input.ts
@@ -1,7 +1,31 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateProductInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Example field (placeholder)' })
+  name: string;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  origin?: string;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  price?: number;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  supplier?: string;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  quantity?: number;
 }
--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,7 +1,31 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Product {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Example field (placeholder)' })
+  name: string;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  origin?: string;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  price?: number;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  supplier?: string;
+
+  @Field(() => String, {
+    description: 'Example field (placeholder)',
+    nullable: true,
+  })
+  quantity?: number;
 }
