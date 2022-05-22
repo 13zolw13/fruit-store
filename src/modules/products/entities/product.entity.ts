3111cd66a23112825f44099a9c9d9d27ab3f5b54
tomek <tomaszpawelkowalski@gmail.com>
2022-05-22 15:20:37 +0200

    feat: update product entity for mongoose schema

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,31 +1,42 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type ProductDocument = Product & Document;
 
 @ObjectType()
+@Schema()
 export class Product {
   @Field(() => String, { description: 'Example field (placeholder)' })
+  @Prop()
   name: string;
 
   @Field(() => String, {
     description: 'Example field (placeholder)',
     nullable: true,
   })
+  @Prop({ required: false })
   origin?: string;
 
   @Field(() => String, {
     description: 'Example field (placeholder)',
     nullable: true,
   })
+  @Prop({ required: false })
   price?: number;
 
   @Field(() => String, {
     description: 'Example field (placeholder)',
     nullable: true,
   })
+  @Prop({ required: false })
   supplier?: string;
 
   @Field(() => String, {
     description: 'Example field (placeholder)',
     nullable: true,
   })
+  @Prop({ required: false })
   quantity?: number;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
