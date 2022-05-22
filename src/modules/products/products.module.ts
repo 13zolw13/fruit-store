057d66a490c3c82cf8bc4f916e5175f04e189419
tomek <tomaszpawelkowalski@gmail.com>
2022-05-22 15:26:22 +0200

    feat: register product schema

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './entities/product.entity';
 import { ProductsResolver } from './products.resolver';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   providers: [ProductsResolver, ProductsService],
 })
 export class ProductsModule {}
