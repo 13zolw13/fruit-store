d6fd9c67013e8a8f9603c4d00384c49156b860b4
tomek <tomaszpawelkowalski@gmail.com>
2022-05-22 15:27:13 +0200

    feat: add new product to db

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProductInput } from './dto/create-product.input';
 import { UpdateProductInput } from './dto/update-product.input';
+import { Product, ProductDocument } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductInput: CreateProductInput) {
-    console.log(createProductInput);
-    return createProductInput;
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
+  async create(createProductInput: CreateProductInput) {
+    const createProduct = new this.productModel(createProductInput);
+
+    return await createProduct.save();
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all products`;
   }
 
