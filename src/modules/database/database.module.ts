bd2ae157a076775045ad51902263deb43f33e0b2
tomek <tomaszpawelkowalski@gmail.com>
2022-05-24 14:58:37 +0200

    feat:  set up db for tests

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URL)],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.NODE_ENV !== 'TEST'
+        ? process.env.DATABASE_URL
+        : process.env.DATABASE_TEST_URL,
+    ),
+  ],
 })
 export class DatabaseModule {}
